b87bbfdd61e6e4e7b3cc92d350734d5b3dd92dc0
Anendra Singh Rajawat <rajawatanendra2306@gmail.com>
2026-08-03 13:59:11 +0530

    Update MapSection.jsx

--- frontend/src/components/MapSection.jsx
+++ frontend/src/components/MapSection.jsx
@@ -5,6 +5,18 @@ import "leaflet-draw";
 import "leaflet.heat";
 import { getListings } from "../api/api";
 
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 function isPointInPolygon(point, polygon) {
   const [lat, lng] = point;
   let inside = false;
