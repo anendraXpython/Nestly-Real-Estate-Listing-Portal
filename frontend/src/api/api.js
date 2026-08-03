3f534d6a0e9e5a68c8bbc970edb5a20f1402a5c0
Anendra Singh Rajawat <rajawatanendra2306@gmail.com>
2026-08-03 13:36:20 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:4000/api";
+const API_BASE = "https://nestly-backend-hf0s.onrender.com/api";
 
 const api = axios.create({ baseURL: API_BASE });
 
